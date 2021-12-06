47e2ed64f44b95f333d872c08fcf0503e37244dd
Nick B <nbourrereg@hotmail.com>
2021-12-06 10:32:54 -0500

    Added comment character in the non significant outputs

--- bno055_sam_mod/bno055_sam_mod.ino
+++ bno055_sam_mod/bno055_sam_mod.ino
@@ -9,15 +9,15 @@ void displaySensorDetails(void)
 {
   sensor_t sensor;
   bno.getSensor(&sensor);
-  Serial.println("------------------------------------");
-  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
-  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
-  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
-  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" xxx");
-  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" xxx");
-  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" xxx");
-  Serial.println("------------------------------------");
-  Serial.println("");
+  Serial.println("#------------------------------------");
+  Serial.print  ("#Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("#Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print  ("#Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.print  ("#Max Value:    "); Serial.print(sensor.max_value); Serial.println(" xxx");
+  Serial.print  ("#Min Value:    "); Serial.print(sensor.min_value); Serial.println(" xxx");
+  Serial.print  ("#Resolution:   "); Serial.print(sensor.resolution); Serial.println(" xxx");
+  Serial.println("#------------------------------------");
+  Serial.println("#");
   delay(500);
 }
 void setup(void)
